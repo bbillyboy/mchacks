4f753fd2d060510444f1a6aeb679b0a279d3c438
Justin <justin.bellavance@gmail.com>
2024-01-28 11:18:17 -0500

    final changes?

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Filters from "./components/Filters";
 import Maps from "./components/Maps";
 import Querybox from "./components/Querybox";
 import FooterBar from "./components/FooterBar";
-import Backend from "./components/Backend"
 
 import "./index.css"
 
@@ -17,10 +16,6 @@ const App = () => {
           <Filters text="Warning" />
           <Filters text="Price" />
           <Filters text="Location" />
-<<<<<<< HEAD
-=======
-          <Backend />
->>>>>>> de1d5aa1cfe5f68afca8e793d0b21e074e347ced
         </div>
         <div style={{ flex: 1 }}>
           <Maps />
